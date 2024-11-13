caf449ce9263789b7a81bdc2417e712a5b584855
Rusu Mhnea-Ionut <rusumihnea23@stud.ase.ro>
2024-11-13 23:39:11 +0200

    Created a very basic command parser that identifies and gives you information about 4 very basic commands

--- OOP Project/OOP Project/main.cpp
+++ OOP Project/OOP Project/main.cpp
@@ -1,5 +1,46 @@
-#include <iostream>
+﻿#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+
+void comanda(char m[256]) {
+	const char* matrix[15][30] = {
+	{"SELECT","SELECT (cel_putin_o_coloana, ...) | ALL FROM nume_tabela [WHERE nume_coloană = valoare]"},
+
+	{"INSERT","INSERT INTO nume_tabela VALUES(...); valorile sunt separate prin, și au numărul și ordinea exacta ca definiția tabelului;"},
+
+	{"UPDATE","UPDATE nume_tabela SET nume_coloană = valoare WHERE nume_coloană = valoare"},
+	{"CREATE","CREATE TABLE table_name [IF NOT EXISTS] ((column_1_name,type,size, default_value), (column_2_name,type,size, default_value), …) - the command should receive at least 1 column;"}
+	};
+
+	for (int j = 0; j < 3; j++) {
+		if (strcmp(m, matrix[j][0]) == 0) {
+			cout << matrix[j][1] << endl;
+			return;
+		}
+	}
+	cout << "Introduceti o comanda valida" << endl;;
+
+
+}
 
 int main() {
-	std::cout << "Commit from visual studio" << std::endl;
+	char input[256];
+	const char valid[5] = "0";
+	cout << "ENTER A COMMAND (0 to stop) : ";
+	cin >> input;
+
+
+	while (strcmp(input, valid) != 0) {
+		if (strcmp(input, valid) != 0)
+			comanda(input);
+		cout << "ENTER A COMMAND (0 to stop) : ";
+		cin >> input;
+
+	}
+
+
+
 }
