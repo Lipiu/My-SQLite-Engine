fa91abe863b5d929d493ea5557ca903404a0d403
Rusu Mhnea-Ionut <rusumihnea23@stud.ase.ro>
2024-12-15 22:29:14 +0200

    Pushed the same thing again

--- OOP Project/OOP Project/main.cpp
+++ OOP Project/OOP Project/main.cpp
@@ -20,8 +20,18 @@ void comanda() {
 
 
 int main() {
+    /*while(true){
+    comanda();
+    }*/
     while (true) {
-        comanda();
+        char* c[20];
+        char comandala[256];
+        cout << "introduceti o comanda: ";
+        cin.get(comandala, 256);
+        cin.get(); //solved the exit code 3
+        Command_parser p(comandala);
+        p.coutVector();
+        //p.validation();
     }
 }
 
