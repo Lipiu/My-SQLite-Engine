cf107cd8f89365ec22aaddc41f14035417e32c85
Rosu Liviu-Mihai <rosuliviu23@stud.ase.ro>
2024-12-15 23:55:05 +0200

    tested the command parser in main, it works

--- OOP Project/OOP Project/main.cpp
+++ OOP Project/OOP Project/main.cpp
@@ -1,40 +1,44 @@
-﻿
-#include"header files/COMMAND_PARSER.h"
+﻿#include"header files/COMMAND_PARSER.h"
 #include <iostream>
 #include <string>
 
-
-
 using namespace std;
 
-void comanda() {
-    char* c[20];
+int main() {
     char comandala[256];
-    cout << "introduceti o comanda: " << endl;
+    cout << "introduceti o comanda: ";
     cin.get(comandala, 256);
     cin.get(); //solved the exit code 3
-    Command_parser p(comandala);
-    //p.coutVector();
-    //p.validation();
-}
 
+    Command_parser p(comandala); // Create the initial object
+    int a = p.getComVal();
 
-int main() {
-    /*while(true){
-    comanda();
-    }*/
-    while (true) {
-        char* c[20];
-        char comandala[256];
+    while (a < 0) {
         cout << "introduceti o comanda: ";
         cin.get(comandala, 256);
-        cin.get(); //solved the exit code 3
-        Command_parser p(comandala);
-        p.coutVector();
-        //p.validation();
+        cin.get();
+
+        // Reuse the existing object and set a new vector
+        p.setVector(comandala);
+        a = p.getComVal();
     }
+
+    cout << p.getComVal() << endl;
+
+    switch (p.getComVal()) {
+    case 0:
+        cout << "select command";
+        break;
+    case 1:
+        cout << "insert command";
+        break;
+    case 2:
+        cout << "update command";
+        break;
+    case 3:
+        cout << "create command";
+        break;
+    }
+
+    return 0;
 }
-
-
-
-
