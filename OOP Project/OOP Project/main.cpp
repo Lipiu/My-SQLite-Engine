c333c0d6441c531a7c12f65bd2b344941591b090
Rosu Liviu-Mihai <rosuliviu23@stud.ase.ro>
2024-12-15 21:36:37 +0200

    now we can enter consecutive right commands

--- OOP Project/OOP Project/main.cpp
+++ OOP Project/OOP Project/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void comanda() {
     char* c[20];
     char comandala[256];
-    cout << "introduceti o comanda: ";
+    cout << "introduceti o comanda: " << endl;
     cin.get(comandala, 256);
     cin.get(); //solved the exit code 3
     Command_parser p(comandala);
@@ -19,7 +19,19 @@ void comanda() {
 
 
 int main() {
+    /*while(true){
     comanda();
+    }*/
+    while (true) {
+        char* c[20];
+        char comandala[256];
+        cout << "introduceti o comanda: ";
+        cin.get(comandala, 256);
+        cin.get(); //solved the exit code 3
+        Command_parser p(comandala);
+        p.coutVector();
+        //p.validation();
+    }
 }
 
 
